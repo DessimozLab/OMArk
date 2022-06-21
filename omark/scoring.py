778f1801dee3b2c2dd20473ded69cc9d048c4026
Yannis Nevers <ynevers@login.curnagl>
2022-06-21 15:43:16 +0200

    Added a license

--- omark/scoring.py
+++ omark/scoring.py
@@ -1,3 +1,18 @@
+'''
+            OMArk - Quality assesment of coding-gene repertoire annotation
+            (C) 2022 Yannis Nevers <yannis.nevers@unil.ch>
+            This file is part of OMArk.
+            OMArk is free software: you can redistribute it and/or modify
+            it under the terms of the GNU Lesser General Public License as published by
+            the Free Software Foundation, either version 3 of the License, or
+            (at your option) any later version.
+            OMArk is distributed in the hope that it will be useful,
+            but WITHOUT ANY WARRANTY; without even the implied warranty of
+            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+            GNU Lesser General Public License for more details.
+            You should have received a copy of the GNU Lesser General Public License
+            along with OMArk. If not, see <http://www.gnu.org/licenses/>.
+        '''
 import omark.omamer_utils as utils
 from omamer.hierarchy import get_descendants
 
