6220cffa13fdfdcfbd6bb3fa1a8acf55139aa2e6
Yannis Nevers <ynevers@login.curnagl>
2022-08-12 18:12:49 +0200

    Bump version: 0.2.0 → 0.2.1

--- omark/__init__.py
+++ omark/__init__.py
@@ -17,7 +17,7 @@
 from datetime import date
 
 __packagename__ = "omark"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __copyright__ = "(C) 2022-{:d} Yannis Nevers  <yannis.nevers@unil.ch>".format(
     date.today().year
 )
